648e43afbcbc0842a1033220584627621fec56e1
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2023-06-15 16:55:53 +0100

    Remove GET /hello endpoint

--- src/hello.js
+++
@@ -1,9 +0,0 @@
-import * as U from "./utils";
-
-export async function handler(/* event, context, callback */) {
-  return U.wrapHandlerImplementation("hello", async () => {
-    const message = "Hello";
-    const url = process.env.BONSAI_URL;
-    return { message, url };
-  });
-};
