85971d4610d6e45dee8b9280d8e3b5ba99e71b18
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2023-06-15 14:05:51 +0100

    Add BONSAI_URL env var

--- src/hello.js
+++ src/hello.js
@@ -3,6 +3,7 @@ import * as U from "./utils";
 export async function handler(/* event, context, callback */) {
   return U.wrapHandlerImplementation("hello", async () => {
     const message = "Hello";
-    return { message };
+    const url = process.env.BONSAI_URL;
+    return { message, url };
   });
 };
