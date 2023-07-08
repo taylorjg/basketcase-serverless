b63f780ee0ebdb287372518cd9377f53550f684f
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2023-07-08 18:33:37 +0100

    Add unit test re bug re facets not taking searchText into account

--- test/search.test.js
+++ test/search.test.js
@@ -31,4 +31,24 @@ describe("search tests", () => {
     expect(builtInFacetValue).toBeDefined();
     expect(builtInFacetValue.count).toBe(12);
   });
+
+  it("search with search text", async () => {
+    const response = await axios.post("/api/search", { searchText: "candy" });
+    const { results, facets } = response.data;
+
+    expect(results.total.value).toBe(4);
+    expect(results.products).toHaveLength(4);
+
+    expect(facets).toHaveLength(4);
+    const fitTypeFacet = findFacet(facets, "Fit Type");
+    expect(fitTypeFacet).toBeDefined();
+
+    const freeStandingFacetValue = findFacetValue(fitTypeFacet, "Free Standing");
+    expect(freeStandingFacetValue).toBeDefined();
+    expect(freeStandingFacetValue.count).toBe(3);
+
+    const builtInFacetValue = findFacetValue(fitTypeFacet, "Built In");
+    expect(builtInFacetValue).toBeDefined();
+    expect(builtInFacetValue.count).toBe(1);
+  });
 });
